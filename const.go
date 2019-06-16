d934855b7cac6ccda32e54176f90ea82f99b8d1b
busydeng <ccong.deng@gmail.com>
2019-06-15 20:28:48 -0400

    add count func

---
+++ const.go
@@ -0,0 +1,16 @@
+package store
+
+type FinanceReportType int
+const (
+	_ FinanceReportType = iota
+	BalanceSheet 			// 资产负债表    
+	CashFlowStatement		// 现金流表
+	IncomeStaement 			// 利润表
+)
+
+type FinanceTimeType int
+const (
+	_ FinanceTimeType = iota
+	Quarter			// 季度 
+	Yearly			// 年度
+)
